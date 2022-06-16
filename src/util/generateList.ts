82e3d6ab0df9b1f60d564493cf95ea95a44e550a
tlendev <oxyaego98@gmail.com>
2022-06-16 11:57:13 +0200

    Add util for mocking items

---
+++ src/util/generateList.ts
@@ -0,0 +1,22 @@
+import { DetailedItem } from '../types';
+
+/**
+ * Utility for mocking x amount of items
+ * @param quantity amount of mocked items to create
+ * @returnsA list of mocked items
+ */
+export const generateMock = (quantity: number) => {
+    const mockItems: Array<DetailedItem> = [];
+    for (let i = 0; i < quantity; i++) {
+        const item = {
+            uid: i.toString(),
+            name: `A BMW ${i}`,
+            quantityLeft: Math.floor(Math.random() * 1000) + 1,
+            description: `A nice BMW ${i} with a nice unique description`,
+            imagePath: `test${Math.floor(Math.random() * 3) + 1}.webp`,
+            priceInPLN: Math.floor(Math.random() * 100000) + 1000,
+        } as DetailedItem;
+        mockItems.push(item);
+    }
+    return mockItems;
+};
