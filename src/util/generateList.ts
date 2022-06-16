d60c08cde50f0360eff573b47bb4b23dcffcdb17
tlendev <oxyaego98@gmail.com>
2022-06-16 16:44:53 +0200

    Add uuid for mocking

--- src/util/generateList.ts
+++ src/util/generateList.ts
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid';
 import { DetailedItem } from '../types';
 
 /**
@@ -9,8 +10,8 @@ export const generateMock = (quantity: number) => {
     const mockItems: Array<DetailedItem> = [];
     for (let i = 0; i < quantity; i++) {
         const item = {
-            uid: i.toString(),
-            name: `A BMW ${i}`,
+            uid: uuid(),
+            name: `BMW ${i}`,
             quantityLeft: Math.floor(Math.random() * 1000) + 1,
             description: `A nice BMW ${i} with a nice unique description`,
             imagePath: `test${Math.floor(Math.random() * 3) + 1}.webp`,
